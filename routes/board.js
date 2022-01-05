b28e9d5dbb3a92853673c6ec3845e6a9898465e1
박세훈 <psh3253@naver.com>
2022-01-05 14:22:28 +0900

    board 수정

--- routes/board.js
+++ routes/board.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const postRouter = require('../routes/post');
-const {Post, User, Board} = require('../models');
+const { sequelize, Post, User, Board} = require('../models');
+const {QueryTypes} = require('sequelize');
 
 router.use('/post', postRouter)
 
 router.get('/:board_id/write', async (req, res) => {
     const board_id = req.params.board_id;
     const board_type = req.query.board_type;
-    const user_id = req.user.id;
+    //const user_id = req.user.id;
+    const user_id = "psh3253"
+
+    res.render("post_write", {
+        board_type: 'general',
+        board_id: board_id
+    })
 });
 
 router.post('/:board_id/write', async (req, res) => {
@@ -16,7 +23,11 @@ router.post('/:board_id/write', async (req, res) => {
     const board_type = req.body.board_type;
     const title = req.body.title;
     const content = req.body.content;
-    const creator_id = req.user.id;
+    console.log(board_id)
+    console.log(board_type)
+    console.log(title)
+    console.log(content)
+    //const creator_id = req.user.id;
 })
 
 router.get('/:board_id', async (req, res, next) => {
@@ -30,15 +41,8 @@ router.get('/:board_id', async (req, res, next) => {
                 id: board_id
             }
         });
-        const posts = await Post.findAll({
-            include: {
-                model: User,
-                attributes: ['nickname']
-            },
-            where: {
-                board_id: board_id
-            },
-            order: [['id', 'ASC']]
+        const posts = await sequelize.query("SELECT post.id, post.title, user.nickname, post.created_at, post.view_count FROM post LEFT JOIN user ON post.creator_id = user.id", {
+            type: QueryTypes.SELECT
         });
         res.render("board", {
                 title: board.name,
