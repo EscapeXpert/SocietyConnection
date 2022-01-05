754c33bc0a48ac565c8bd011123be1d81d2bff7e
박세훈 <psh3253@naver.com>
2022-01-05 17:57:50 +0900

    board 수정 및 post 추가

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');
 
 module.exports = class User extends Sequelize.Model {
-    static init(sequelize){
+    static init(sequelize) {
         return super.init({
             id: {
                 type: Sequelize.STRING(30),
@@ -63,16 +63,17 @@ module.exports = class User extends Sequelize.Model {
     }
 
     static associate(db) {
-        db.User.hasMany(db.Message, { foreignKey: 'sender_id', sourceKey: 'id'});
-        db.User.hasMany(db.Message, { foreignKey: 'receiver_id', sourceKey: 'id'});
-        db.User.hasMany(db.Comment, { foreignKey: 'creator_id', sourceKey: 'id'});
-        db.User.hasMany(db.Post, { foreignKey: 'creator_id', sourceKey: 'id'});
-        db.User.hasMany(db.ReplyComment, { foreignKey: 'creator_id', sourceKey: 'id'});
-        db.User.hasMany(db.Applicant, { foreignKey: 'user_id', sourceKey: 'id'});
-        db.User.hasMany(db.Recruitment, { foreignKey: 'creator_id', sourceKey: 'id'});
+        db.User.hasMany(db.Message, {foreignKey: 'sender_id', sourceKey: 'id'});
+        db.User.hasMany(db.Message, {foreignKey: 'receiver_id', sourceKey: 'id'});
+        db.User.hasMany(db.Comment, {foreignKey: 'creator_id', sourceKey: 'id'});
+        db.User.hasMany(db.Post, {foreignKey: 'creator_id', sourceKey: 'id'});
+        db.User.hasMany(db.ReplyComment, {foreignKey: 'creator_id', sourceKey: 'id'});
+        db.User.hasMany(db.Applicant, {foreignKey: 'user_id', sourceKey: 'id'});
+        db.User.hasMany(db.Recruitment, {foreignKey: 'creator_id', sourceKey: 'id'});
         db.User.belongsToMany(db.Post, {
             foreignKey: 'user_id',
             through: 'Like',
+            timestamps: false
         });
     }
 };
--- routes/board.js
+++ routes/board.js
@@ -1,54 +1,75 @@
 const express = require('express');
 const router = express.Router();
 const postRouter = require('../routes/post');
-const { sequelize, Post, User, Board} = require('../models');
+const {sequelize, Post, Board} = require('../models');
 const {QueryTypes} = require('sequelize');
 
-router.use('/post', postRouter)
+router.use('board/1/post', postRouter);
 
-router.get('/:board_id/write', async (req, res) => {
+router.get('/:board_id/write', async (req, res, next) => {
     const board_id = req.params.board_id;
-    const board_type = req.query.board_type;
-    //const user_id = req.user.id;
-    const user_id = "psh3253"
-
-    res.render("post_write", {
-        board_type: 'general',
-        board_id: board_id
-    })
-});
-
-router.post('/:board_id/write', async (req, res) => {
-    const board_id = req.params.board_id;
-    const board_type = req.body.board_type;
-    const title = req.body.title;
-    const content = req.body.content;
-    console.log(board_id)
-    console.log(board_type)
-    console.log(title)
-    console.log(content)
-    //const creator_id = req.user.id;
-})
-
-router.get('/:board_id', async (req, res, next) => {
-    const board_id = req.params.board_id;
-    const board_type = req.query.board_type;
-    const page = req.query.page;
     try {
         const board = await Board.findOne({
-            attributes: ['name'],
+            attributes: ['id', 'board_type', 'name'],
             where: {
                 id: board_id
             }
         });
-        const posts = await sequelize.query("SELECT post.id, post.title, user.nickname, post.created_at, post.view_count FROM post LEFT JOIN user ON post.creator_id = user.id", {
-            type: QueryTypes.SELECT
-        });
-        res.render("board", {
-                title: board.name,
-                posts: posts
+        res.render('post_write', {
+            board: board
+        })
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+});
+
+router.post('/:board_id/write', async (req, res, next) => {
+    const board_id = req.params.board_id;
+    const board_type = req.body.board_type;
+    const title = req.body.title;
+    const content = req.body.content;
+    const creator_id = 'psh3253';
+    //const creator_id = req.user.id;
+    try {
+        if(board_type === 'general')
+        {
+            await Post.create({
+                title: title,
+                content: content,
+                board_id: board_id,
+                creator_id: creator_id
+            });
+        }
+        res.redirect(`/board/${board_id}`);
+    } catch (err) {
+        console.error(err);
+        next(err);
+    }
+})
+
+router.get('/:board_id', async (req, res, next) => {
+    const board_id = req.params.board_id;
+    const page = req.query.page;
+    try {
+        const board = await Board.findOne({
+            attributes: ['id', 'board_type', 'name'],
+            where: {
+                id: board_id
             }
-        );
+        });
+        if (board.board_type === 'general') {
+            const posts = await sequelize.query('SELECT post.id, post.title, user.nickname, post.created_at, post.view_count, (SELECT count(*) FROM `like` WHERE post_id = post.id) `like` FROM `post` LEFT JOIN `user` ON post.creator_id = user.id', {
+                type: QueryTypes.SELECT
+            });
+            res.render('board', {
+                    board: board,
+                    posts: posts,
+                }
+            );
+        } else if (board.board_type === 'recruitment') {
+
+        }
     } catch (err) {
         console.error(err);
         next(err);
