08e8024464faba537b0ff27d8fd16576e8921438
마상균 <wodon326@naver.com>
2022-01-19 17:45:46 +0900

    메인 수정 및 프로필 수정

--- routes/main.js
+++ routes/main.js
@@ -2,15 +2,22 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const {isLoggedIn, isNotLoggedIn} = require('./middlewares');
 const User = require('../models/user');
+const Board = require('../models/board');
 const passport = require("passport");
+const {Op} = require("sequelize");
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
     console.log(req.user);
+    const BoardList = await Board.findAll({
+        attributes: ['id','name']
+    });
+    console.log(BoardList);
     res.render('main', {
         title: '메인' ,
         User: req.user,
+        BoardList:BoardList
     });
 });
 module.exports = router;
