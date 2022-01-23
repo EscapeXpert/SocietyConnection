a4d228cf44b5dba79c1eace2ff7802b9f3cc3e01
wodon326 <wodon326@naver.com>
2022-01-23 19:39:37 +0900

    게시판 생성 및 수정 삭제 grade 수정 및 회원 닉네임 및 등급 수정 회원탈퇴(대충 구현만 해둠)

--- routes/admin.js
+++ routes/admin.js
@@ -2,8 +2,10 @@ const express = require('express');
 const passport = require('passport');
 const {isLoggedIn, isNotLoggedIn} = require('./middlewares');
 const User = require("../models/user");
+const Grade = require("../models/grade");
 const bcrypt = require("bcrypt");
 const {Op} = require("sequelize");
+const Board = require("../models/board");
 const router = express.Router();
 
 router.get('/', isLoggedIn, async (req, res) => {
@@ -16,13 +18,134 @@ router.get('/', isLoggedIn, async (req, res) => {
             }
         }
     });
+    const BoardList = await Board.findAll();
     res.render('admin', {
         title: 'admin',
-        UserList : UserList
+        UserList : UserList,
+        BoardList : BoardList
     });
 });
+router.get('/:User_nickname/edit', isLoggedIn, async (req, res) => {
+    if(req.user.grade!==5){
+        return res.send('<script> alert("admin이 아닙니다.");window.location.replace("/");</script>');
+    }
+    const User_nickname = req.params.User_nickname;
+    const FindUser = await User.findOne({ where: { nickname : User_nickname } });
+    const GradeList = await Grade.findAll({
+        order: [['id']]
+    });
+    res.render('admin_edit', {
+        title: 'admin_edit',
+        FindUser: FindUser,
+        GradeList : GradeList
+    });
+});
+router.post('/:User_nickname/edit', isLoggedIn, async (req, res, next) => {
+    if(req.user.grade!==5){
+        return res.send('<script> alert("admin이 아닙니다.");window.location.replace("/");</script>');
+    }
+    const User_nickname = req.params.User_nickname;
+    const {nickname,grade}= req.body;
+    try {
+        if (!nickname) {
+            return res.send('<script> alert("닉네임을 입력해주세요.");history.back()</script>');
+        }
+        exUser = await User.findOne({where: {nickname : nickname}});
+        if (exUser&&exUser.nickname!==User_nickname) {
+            return res.send('<script> alert("이미 존재하는 닉네임입니다.");history.back()</script>');
+        }
+        await User.update({
+            nickname : nickname,
+            grade: grade
+        }, {
+            where: {nickname : User_nickname},
+        });
+        res.redirect(`/admin`);
+    } catch (error) {
+        console.error(error);
+        return next(error);
+    }
+});
 
+router.get('/board_create', isLoggedIn, async (req, res) => {
+    if(req.user.grade!==5){
+        return res.send('<script> alert("admin이 아닙니다.");window.location.replace("/");</script>');
+    }const GradeList = await Grade.findAll({
+        order: [['id']]
+    });
+    res.render('admin_board_create', {
+        title: 'admin_board_create',
+        GradeList : GradeList
+    });
+});
+router.post('/board_create', isLoggedIn, async (req, res, next) => {
+    if(req.user.grade!==5){
+        return res.send('<script> alert("admin이 아닙니다.");window.location.replace("/");</script>');
+    }
+    const {name,min_read_grade,min_write_grade,board_type}= req.body;
+    try {
+        await Board.create({
+            name : name,
+            min_read_grade: min_read_grade,
+            min_write_grade : min_write_grade,
+            board_type : board_type
+        });
+        res.redirect(`/admin`);
+    } catch (error) {
+        console.error(error);
+    }
+});
 
+router.get('/:Board_id/board_edit', isLoggedIn, async (req, res) => {
+    if(req.user.grade!==5){
+        return res.send('<script> alert("admin이 아닙니다.");window.location.replace("/");</script>');
+    }
+    const Board_id = req.params.Board_id;
+    const FindBoard = await Board.findOne({where: {id : Board_id}});
+    const GradeList = await Grade.findAll({
+        order: [['id']]
+    });
+    res.render('admin_board_edit', {
+        title: 'admin_edit',
+        FindBoard: FindBoard,
+        GradeList : GradeList
+    });
+});
+router.post('/:Board_id/board_edit', isLoggedIn, async (req, res, next) => {
+    if(req.user.grade!==5){
+        return res.send('<script> alert("admin이 아닙니다.");window.location.replace("/");</script>');
+    }
+    const Board_id = req.params.Board_id;
+    const {name,min_read_grade,min_write_grade,board_type}= req.body;
+    try {
+        await Board.update({
+            name : name,
+            min_read_grade: min_read_grade,
+            min_write_grade : min_write_grade,
+            board_type : board_type
+        }, {
+            where: {id : Board_id},
+        });
+        res.redirect(`/admin`);
+    } catch (error) {
+        console.error(error);
+        return next(error);
+    }
+});
+router.get('/:Board_id/board_delete', isLoggedIn, async (req, res) => {
+    if(req.user.grade!==5){
+        return res.send('<script> alert("admin이 아닙니다.");window.location.replace("/");</script>');
+    }
+    const Board_id = req.params.Board_id;
+    try {
+        await Board.destroy({
+            where: {id: Board_id}
+        });
+        res.redirect(`/admin`);
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 
 
--- routes/profile.js
+++ routes/profile.js
@@ -6,7 +6,8 @@ const moment = require('moment');
 const fs = require("fs");
 const path = require("path");
 const multer = require("multer");
-const { Op } = require("sequelize");
+const {Op} = require("sequelize");
+const axios = require("axios");
 const router = express.Router();
 
 
@@ -18,12 +19,12 @@ try {
 }
 
 
-router.get('/:user_nickname', isLoggedIn, async(req, res) => {
-    const Find_User = await User.findOne({ where: { nickname : req.params.user_nickname } });
+router.get('/:user_nickname', isLoggedIn, async (req, res) => {
+    const Find_User = await User.findOne({where: {nickname: req.params.user_nickname}});
     res.render('profile', {
-        title: '프로필' ,
+        title: '프로필',
         User: Find_User,
-        req_User : req.user
+        req_User: req.user
     });
 });
 
@@ -37,24 +38,24 @@ const upload = multer({
             cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
         },
     }),
-    limits: { fileSize: 5 * 1024 * 1024 },
+    limits: {fileSize: 5 * 1024 * 1024},
 });
 
-router.post('/:user_nickname/edit/img', isLoggedIn, upload.single('img'), async(req, res) => {
+router.post('/:user_nickname/edit/img', isLoggedIn, upload.single('img'), async (req, res) => {
     const user_nickname = req.params.user_nickname;
-    if (user_nickname!==req.user.nickname) {
+    if (user_nickname !== req.user.nickname) {
         return res.send('<script> alert("잘못된 접근입니다.");history.back()</script>');
     }
-    res.json({ url: `/img/${req.file.filename}` });
+    res.json({url: `/img/${req.file.filename}`});
 });
 
-router.get('/:user_nickname/edit', isLoggedIn, async(req, res) => {
+router.get('/:user_nickname/edit', isLoggedIn, async (req, res) => {
     const user_nickname = req.params.user_nickname;
-    if (user_nickname!==req.user.nickname) {
+    if (user_nickname !== req.user.nickname) {
         return res.send('<script> alert("잘못된 접근입니다.");history.back()</script>');
     }
     res.render('edit', {
-        title: '프로필 수정' ,
+        title: '프로필 수정',
         User: req.user,
         birth: moment(req.user.birth_date).format('YYYY-MM-DD')
     });
@@ -63,27 +64,27 @@ router.get('/:user_nickname/edit', isLoggedIn, async(req, res) => {
 const upload2 = multer();
 router.post('/:user_nickname/edit', isLoggedIn, upload2.none(), async (req, res, next) => {
     const user_nickname = req.params.user_nickname;
-    if (user_nickname!==req.user.nickname) {
+    if (user_nickname !== req.user.nickname) {
         return res.send('<script> alert("잘못된 접근입니다.");history.back()</script>');
     }
-    const {sns_id, nickname,name,birth_date,gender,introduce,profile_image }= req.body;
+    const {sns_id, nickname, name, birth_date, gender, introduce, profile_image} = req.body;
 
     try {
         if (!nickname) {
             return res.send('<script> alert("닉네임을 입력해주세요.");history.back()</script>');
         }
-        exUser = await User.findOne({where: {nickname : nickname}});
-        if (exUser&&exUser.nickname!==req.user.nickname) {
+        exUser = await User.findOne({where: {nickname: nickname}});
+        if (exUser && exUser.nickname !== req.user.nickname) {
             return res.send('<script> alert("이미 존재하는 닉네임입니다.");history.back()</script>');
         }
         await User.update({
-            sns_id : sns_id,
-            nickname : nickname,
-            name : name,
-            birth_date : birth_date,
-            gender : gender,
-            introduce : introduce,
-            profile_image : profile_image
+            sns_id: sns_id,
+            nickname: nickname,
+            name: name,
+            birth_date: birth_date,
+            gender: gender,
+            introduce: introduce,
+            profile_image: profile_image
         }, {
             where: {id: req.user.id},
         });
@@ -95,30 +96,29 @@ router.post('/:user_nickname/edit', isLoggedIn, upload2.none(), async (req, res,
 });
 
 
-
-router.get('/:user_nickname/change_password', isLoggedIn, async(req, res) => {
+router.get('/:user_nickname/change_password', isLoggedIn, async (req, res) => {
     const user_nickname = req.params.user_nickname;
-    if (user_nickname!==req.user.nickname) {
+    if (user_nickname !== req.user.nickname) {
         return res.send('<script> alert("잘못된 접근입니다.");history.back()</script>');
     }
     res.render('change_password', {
         title: '비밀번호 변경',
-        user_nickname : user_nickname
+        user_nickname: user_nickname
     });
 });
 router.post('/:user_nickname/change_password', isLoggedIn, async (req, res, next) => {
     const user_nickname = req.params.user_nickname;
-    if (user_nickname!==req.user.nickname) {
+    if (user_nickname !== req.user.nickname) {
         return res.send('<script> alert("잘못된 접근입니다.");history.back()</script>');
     }
-    const {password, new_password,verify_new_password} = req.body;
+    const {password, new_password, verify_new_password} = req.body;
 
     try {
         const result = await bcrypt.compare(password, req.user.password);
         if (!result) {
             return res.send('<script> alert("기존 비밀번호가 일치하지 않습니다.");history.back()</script>');
         }
-        if (new_password!==verify_new_password) {
+        if (new_password !== verify_new_password) {
             return res.send('<script> alert("새 비밀번호와 비밀번호 확인이 일치하지 않습니다.");history.back()</script>');
         }
         if (password === new_password) {
@@ -137,5 +137,32 @@ router.post('/:user_nickname/change_password', isLoggedIn, async (req, res, next
     }
 });
 
+router.get('/:user_nickname/account_delete', isLoggedIn, async (req, res) => {
+    const user_nickname = req.params.user_nickname;
+    if (user_nickname !== req.user.nickname) {
+        return res.send('<script> alert("잘못된 접근입니다.");history.back()</script>');
+    }
+    try {
+        await User.destroy({
+            where: {nickname: user_nickname}
+        });
+        if (req.user.login_type === 'kakao') {
+            let logout = await axios({
+                method: 'post',
+                url: 'https://kapi.kakao.com/v1/user/unlink',
+                headers: {
+                    'Authorization': `Bearer ${req.user.accessToken}`
+                }
+            });
+        }
+        req.logout();
+        if (req.session) {
+            req.session.destroy();
+        }
+        res.redirect(`/`);
+    } catch (error) {
+        console.error(error);
+    }
+});
 
 module.exports = router;
