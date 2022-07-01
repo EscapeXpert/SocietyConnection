20c29fd62d2e79fccdd4d6eab2a2b255aa7c2f9c
minus <aaaa6658@naver.com>
2022-07-01 19:21:21 +0900

    닉네임 특수문자 금지 추가

--- routes/admin.js
+++ routes/admin.js
@@ -64,7 +64,7 @@ router.post('/:User_nickname/edit', csrfProtection, isLoggedIn, async (req, res,
         if (exUser && exUser.nickname !== User_nickname) {
             return res.send('<script> alert("이미 존재하는 닉네임입니다.");history.back();</script>');
         }
-        const nicknameRules = /^(?=.*[a-zA-Z])(?=.*[0-9]).{,30}$/;
+        const nicknameRules = /^[\w+]{1,30}$/;
         if(!nicknameRules.test(nickname)) {
             return res.send('<script> alert("닉네임은 특수문자 제외 30자까지 가능합니다.");history.back()</script>');
         }
